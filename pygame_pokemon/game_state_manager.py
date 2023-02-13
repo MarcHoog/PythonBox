3bc4d40c3e8f2680b9d07691769fb6c11d432ced
marc <marc.hoogendoorn@simac.com/bin/python3.11>
2023-02-13 13:01:39 +0100

    pretty :D

--- pygame_pokemon/game_state_manager.py
+++ pygame_pokemon/game_state_manager.py
@@ -29,32 +29,6 @@ class GameState(ABC):
     @abstractmethod
     def draw(self):
         pass
-
-class GameStateManager:
-    def __init__(self):
-        self.states = {'main_menu': MainMenu,
-                       'game': Game,}
-
-        self.state = None
-        self.default = 'game'
-        
-        self.display = pygame.display.get_surface()
-        self.player = 'player'
-        
-        self.change_state(self.default)
-
-
-    def change_state(self, new_state):
-        if self.state:
-            self.state.exit()
-        self.state = self.states[new_state](self.display)
-        self.state.enter()
-
-    def run(self):
-        if self.state.transition and self.state.transition != self.state:
-            self.change_state(self.state.transition)
-        self.state.update()
-        self.state.draw()
         
 class Game(GameState):
 
@@ -100,3 +74,31 @@ class MainMenu(GameState):
 
     def draw():
         pass
+    
+class GameStateManager:
+    def __init__(self):
+        self.states = {'main_menu': MainMenu,
+                       'game': Game,}
+
+        self.state = None
+        self.default = 'game'
+        
+        self.display = pygame.display.get_surface()
+        
+        # We do this so that the player can be accesed from any state, 
+        self.player = 'player'
+        
+        self.change_state(self.default)
+
+
+    def change_state(self, new_state):
+        if self.state:
+            self.state.exit()
+        self.state = self.states[new_state](self.display)
+        self.state.enter()
+
+    def run(self):
+        if self.state.transition and self.state.transition != self.state:
+            self.change_state(self.state.transition)
+        self.state.update()
+        self.state.draw()
